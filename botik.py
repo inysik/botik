186a52ea970a21c5ef70663d75efbd7f5ad78fdf
InessaRyzhikova <inessa1992@yandex.ru>
2023-04-14 23:26:39 +0300

    rabotae2

--- botik.py
+++ botik.py
@@ -96,7 +96,10 @@ def show_board():
     row3 = f'{div} {board[2][0]} {div} {board[2][1]} {div} {board[2][2]} {div}'
 
     # Отправляем игровое поле в чат
-    bot.send_message(chat_id, f'{top}\n{row1}\n{mid}\n{row2}\n{mid}\n{row3}\n{bot}')
+    # bot.send_message(chat_id, f'{top}\n{row1}\n{mid}\n{row2}\n{mid}\n{row3}\n{bot}')
+    # bot.send_message(chat_id, game_board, parse_mode='Markdown')
+    game_board = f'{top}\n{row1}\n{mid}\n{row2}\n{mid}\n{row3}\n{bot}'
+    # Отправляем игровое поле в чат
     bot.send_message(chat_id, game_board, parse_mode='Markdown')
 
 # Запускаем бота
